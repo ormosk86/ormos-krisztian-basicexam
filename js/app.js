b927ab7beeda2aae616173025f9def8f7ede5235
Ormos Krisztian <ok86@hotmail.com>
2018-06-11 16:52:16 +0200

    third

--- js/app.js
+++ js/app.js
@@ -12,124 +12,132 @@ function getData(url, callbackFunc) {
 function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
-  console.log(userDatas);
-  sortSpaceships(userDatas);
-  createHtmlElements(userDatas);
 
-  var searchbuttonElement = document.querySelector('#search-button');
-  searchbuttonElement.addEventListener('click', function () {
-    searchCharacter(userDatas);
-  }, false);
+  // 1. feladat
+  function sortSpaceships(spaceshipsArray) {
+    spaceshipsArray = spaceshipsArray.sort(function (lho, rho) {
+      var result;
 
-  statistic(userDatas);
-}
-
-getData('/json/spaceshipsArray.json', successAjax);
-
-
-// 1. feladat
-function sortSpaceships(spaceshipsArray) {
-  spaceshipsArray = spaceshipsArray.sort(function (lho, rho) {
-    var result;
-
-    if (lho.cost_in_credits > rho.cost_in_credits) {
-      result = 1;
-    } else if (lho.cost_in_credits == rho.cost_in_credits) {
-      result = 0;
-    } else if (lho.cost_in_credits < rho.cost_in_credits) {
-      result = -1;
-    }
-    return result;
-  });
-};
+      if (lho.cost_in_credits > rho.cost_in_credits) {
+        result = 1;
+      } else if (lho.cost_in_credits == rho.cost_in_credits) {
+        result = 0;
+      } else if (lho.cost_in_credits < rho.cost_in_credits) {
+        result = -1;
+      }
+      return result;
+    });
+  };
 
 
-// 2. feladat
+  // 2. feladat
 
-function consumablesNotNull(spaceshipsArray) {
-  for (var i = spaceshipsArray.length - 1; i >= 0; --i) {
-    if (spaceshipsArray[i].consumables == null) {
-      spaceshipsArray.splice(i, 1);
-    }
-  }
-  return spaceshipsArray;
-}
-
-
-
-// 3. feladat
-function nullToUnknown(spaceshipsArray) {
-  for (var i = 0; i < spaceshipsArray.length; i++) {
-    for (var j in spaceshipsArray[i]) {
-      if (spaceshipsArray[i][j] === null) {
-        spaceshipsArray[i][j] = "unknown";
+  function consumablesNotNull(spaceshipsArray) {
+    for (var i = spaceshipsArray.length - 1; i >= 0; --i) {
+      if (spaceshipsArray[i].consumables == null) {
+        spaceshipsArray.splice(i, 1);
       }
     }
-  }
-  return spaceshipsArray;
-}
-
-
-
-// 4. feladat
-function createHtmlElements(spaceshipsArray) {
-  var spaceshipListDivElement = document.querySelector(".shapceship-list");
-
-  for (var i = 0; i < spaceshipsArray.length; i++) {
-
-    var cardDivElement = document.createElement('div');
-    cardDivElement.classList.add("card");
-    spaceshipListDivElement.appendChild(cardDivElement);
-
-    var cardDivElementImage = document.createElement("img");
-    cardDivElementImage.classList.add("image-spaceship");
-    cardDivElement.appendChild(cardDivElementImage);
-    cardDivElementImage.src = "img/" + spaceshipsArray[i].image;
-    cardDivElementImage.alt = spaceshipsArray[i].model;
-
-    var cardDivElementName = document.createElement("div");
-    cardDivElementName.classList.add("model-spaceship");
-    cardDivElement.appendChild(cardDivElementName);
-    cardDivElementName.innerText = spaceshipsArray[i].model;
-
-    var cardDivElementDetails = document.createElement("div");
-    cardDivElementDetails.classList.add("model-spaceship");
-    cardDivElement.appendChild(cardDivElementDetails);
-
-    var s = '';
-    for (var member in spaceshipsArray[i]) {
-      s += member + ': ' + spaceshipsArray[i][member] + '<br/>';
-    }
-    cardDivElementDetails.innerHTML = s;
-  }
-};
-
-//5. feladat
-function statistic(spaceshipsArray) {
-  var spaceshipListDivElement = document.querySelector(".shapceship-list");
-
-  var darab = 0;
-  for (var i = 0; i < spaceshipsArray.length; i++) {
-    if (spaceshipsArray[i].crew == 1) {
-      darab++;
-    }
+    return spaceshipsArray;
   }
 
-  var max = spaceshipsArray[0].cargo_capacity;
-  for (var i = 1; i < spaceshipsArray.length; i++) {
-    if (spaceshipsArray[i].cargo_capacity > max) {
-      max = spaceshipsArray[i];
+
+
+  // 3. feladat
+  function nullToUnknown(spaceshipsArray) {
+    for (var i = 0; i < spaceshipsArray.length; i++) {
+      for (var j in spaceshipsArray[i]) {
+        if (spaceshipsArray[i][j] === null) {
+          spaceshipsArray[i][j] = "unknown";
+        }
+      }
     }
+    return spaceshipsArray;
+  }
+
+
+
+  // 4. feladat
+  function createHtmlElements(spaceshipsArray) {
+    var spaceshipListDivElement = document.querySelector(".shapceship-list");
+
+    for (var i = 0; i < spaceshipsArray.length; i++) {
+
+      var cardDivElement = document.createElement('div');
+      cardDivElement.classList.add("card");
+      spaceshipListDivElement.appendChild(cardDivElement);
+
+      var cardDivElementPhoto = document.createElement("img");
+      cardDivElementPhoto.classList.add("image-spaceship");
+      cardDivElement.appendChild(cardDivElementPhoto);
+      cardDivElementPhoto.src = "img/" + spaceshipsArray[i].image;
+      cardDivElementPhoto.alt = spaceshipsArray[i].model;
+
+      var cardDivElementName = document.createElement("div");
+      cardDivElementName.classList.add("model-spaceship");
+      cardDivElement.appendChild(cardDivElementName);
+      cardDivElementName.innerText = spaceshipsArray[i].model;
+
+      var cardDivElementProperties = document.createElement("div");
+      cardDivElementProperties.classList.add("model-spaceship");
+      cardDivElement.appendChild(cardDivElementProperties);
+
+      var k = '';
+      for (var element in spaceshipsArray[i]) {
+        k += element + ': ' + spaceshipsArray[i][element] + '<br/>';
+      }
+      cardDivElementProperties.innerHTML = k;
+    }
+  };
+
+  //5. feladat
+  function statistic(spaceshipsArray) {
+    var spaceshipListDivElement = document.querySelector(".shapceship-list");
+
+    var darab = 0;
+    for (var i = 0; i < spaceshipsArray.length; i++) {
+      if (spaceshipsArray[i].crew == 1) {
+        darab++;
+      }
+    }
+
+    var max = spaceshipsArray[0].cargo_capacity;
+    for (var i = 1; i < spaceshipsArray.length; i++) {
+      if (spaceshipsArray[i].cargo_capacity > max) {
+        max = spaceshipsArray[i];
+      }
+    };
+    var passangersSum = 0;
+    for (var i = 0; i < spaceships.length; i++) {
+      passangersSum += parseInt(spaceships[i].passengers);
+    };
+
+    var lengthinessShip = spaceships[0].lengthiness;
+    for (var i = 0; i < szamokhalmaza.length; i++) {
+      if (spaceships[i].lengthiness < lengthinessShip) {
+        lengthinessShip = spaceships[i].lengthiness;
+      }
+    };
+
+    var StatisticDivElement = document.createElement('div');
+    spaceshipListDivElement.appendChild(StatisticDivElement);
+    StatisticDivElement.innerHTML = "Egy fős legénységgel rendelkező hajók darabszáma: " + darab + "<br>" + "A legnagyobb cargo_capacity-vel rendelkező hajó neve: " + max + "<br>"
+
   };
 
 
 
-  var StatisticDivElement = document.createElement('div');
-  spaceshipListDivElement.appendChild(StatisticDivElement);
-  StatisticDivElement.innerHTML = "Egy fős legénységgel rendelkező hajók darabszáma: " + darab + "<br>" + "A legnagyobb cargo_capacity-vel rendelkező hajó neve: " + max + "<br>"
+  //6.feladat
 
+
+
+
+
+  sortSpaceships(userDatas);
+  consumablesNotNull(userDatas);
+  nullToUnknown(userDatas);
+  createHtmlElements(userDatas);
+  statistic(userDatas);
 };
 
-
-
-//6.feladat
+getData('/json/spaceshipsArray.json', successAjax);
